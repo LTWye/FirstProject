20d6da4a1422055c30ce087aae7c6cbbc95e5647
LTWye <30402882+LTWye@users.noreply.github.com>
2017-07-24 15:45:46 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package com.briup.day01;
+
+import java.util.Date;
+import java.text.DateFormat;
+
+public class Hello{
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		
+		System.out.println("Hello world change...");
+		System.out.println(d);
+	}
+}
